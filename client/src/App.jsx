10c422af4313cc4e4bc293bdf35287577eba5c77
arturokaadu <arturokaadu2@gmail.com>
2022-09-22 04:22:45 -0300

    'initial authentication and login'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,22 +7,34 @@ import { About } from "./pages/About/About.jsx";
 import { Detail } from "./pages/Detail/Detail.jsx";
 import { Navigate } from "react-router-dom";
 import { Home } from "./pages/Home/Home.jsx";
+import { LogIn } from "./pages/LogIn/LogIn.jsx";
+
+import { AuthProvider } from "./pages/firebase/context.jsx";
+import { Dashboard } from "./pages/Dashboard/Dashboard.jsx";
+import { Register } from "./pages/Register/Register.jsx";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/catalogue" element={<Catalogue />} />
-        <Route path="/createproduct" element={<CreateProduct />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          {/* added ProtectedRoute removed navigate */}
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* <Route path="/home" element={<Home/>} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/catalogue" element={<Catalogue />} />
+          <Route path="/createproduct" element={<CreateProduct />} />
+          <Route path="/about" element={<About />} /> */}
+          <Route path="/Login" element={<LogIn />} />
+          <Route path="/register" element={<Register/>} />
+        </Routes>
+      </AuthProvider>
       <Footer />
     </BrowserRouter>
   );
 };
 
 export default App;
+
